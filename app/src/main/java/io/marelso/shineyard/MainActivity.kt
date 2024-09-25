2fc5673889ad7f7fc69a00935b7144d4f4fa0a5c
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-09-25 18:38:44 -0300

    Including required annotation

--- app/src/main/java/io/marelso/shineyard/MainActivity.kt
+++ app/src/main/java/io/marelso/shineyard/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
  import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
