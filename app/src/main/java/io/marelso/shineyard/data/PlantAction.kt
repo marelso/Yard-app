cc421bc0b0d88fb0bb97f842288e7aa33fa6e645
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-06 19:29:40 -0300

    including new data class to handle plant actions

--- app/src/main/java/io/marelso/shineyard/data/PlantAction.kt
+++ app/src/main/java/io/marelso/shineyard/data/PlantAction.kt
@@ -3,7 +3,23 @@ package io.marelso.shineyard.data
 import io.marelso.shineyard.R
 
 enum class PlantAction(
-    val title: Int, var onClick: (() -> Unit)? = null
+    val title: Int,
+    val icon: Int,
+    val empty: Int,
+    val cta: Int,
+    var onCreate: (() -> Unit)? = null,
+    var onEdit: (() -> Unit)? = null
 ) {
-    SCHEDULE(R.string.title_action_schedule_watering)
+    SCHEDULE(
+        title = R.string.title_action_schedule,
+        icon = R.drawable.ic_schedule,
+        empty = R.string.title_action_schedule_empty,
+        cta = R.string.title_action_schedule_watering
+    ),
+    NOTIFICATION(
+        title = R.string.title_action_notification,
+        icon = R.drawable.ic_notification,
+        empty = R.string.title_action_notification_empty,
+        cta = R.string.title_action_schedule_notification
+    )
 }
