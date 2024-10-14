5241e580b3b0114a36ffce7b17a1d030a958957c
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 18:58:47 -0300

    Initializing firebase

--- app/src/main/java/io/marelso/shineyard/ShineYardApp.kt
+++ app/src/main/java/io/marelso/shineyard/ShineYardApp.kt
@@ -1,6 +1,7 @@
 package io.marelso.shineyard
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import io.marelso.shineyard.di.shineYardModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext
@@ -8,7 +9,7 @@ import org.koin.core.context.GlobalContext
 class ShineYardApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        FirebaseApp.initializeApp(this)
         GlobalContext.startKoin {
             androidContext(this@ShineYardApp)
             modules(shineYardModule)
