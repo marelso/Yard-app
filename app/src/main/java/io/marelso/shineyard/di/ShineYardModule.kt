4cd00a7407598da16541bd6063b2a4ade594b35f
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 18:59:25 -0300

    Initializing firebase

--- app/src/main/java/io/marelso/shineyard/di/ShineYardModule.kt
+++ app/src/main/java/io/marelso/shineyard/di/ShineYardModule.kt
@@ -1,17 +1,23 @@
 package io.marelso.shineyard.di
 
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.database.FirebaseDatabase
 import io.marelso.shineyard.data.network.FirebaseRepository
 import io.marelso.shineyard.data.network.networkModule
 import io.marelso.shineyard.ui.detail.DetailViewModel
 import io.marelso.shineyard.ui.login.LoginViewModel
 import io.marelso.shineyard.ui.login.di.loginModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val shineYardModule = module {
     factory {
         FirebaseRepository(database = FirebaseDatabase.getInstance().getReference("sensor"))
+    }
+
+    factory {
+        FirebaseAnalytics.getInstance(androidContext())
     }
 
     viewModelOf(::DetailViewModel)
