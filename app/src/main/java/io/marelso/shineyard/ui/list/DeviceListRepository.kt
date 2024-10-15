8c4b9aea2759d4ee25cd3fedb3fbcd55e507a769
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-15 08:09:17 -0300

    Including new methods to fetch devices by id

--- app/src/main/java/io/marelso/shineyard/ui/list/DeviceListRepository.kt
+++ app/src/main/java/io/marelso/shineyard/ui/list/DeviceListRepository.kt
@@ -1,4 +1,20 @@
 package io.marelso.shineyard.ui.list
 
-class DeviceListRepository {
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import io.marelso.shineyard.data.Device
+import io.marelso.shineyard.data.toEntity
+
+class DeviceListRepository(private val database: DatabaseReference) {
+    fun getDevice(deviceId: String, onDataChange: (Device?) -> Unit) {
+        database.child(deviceId).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                onDataChange(snapshot.getValue(Device::class.java))
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
 }
