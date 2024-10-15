5918dbfc636937ccb17ff4d0ca6a359356f8c042
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-15 08:09:47 -0300

    Injecting new repository and fetching account devices

--- app/src/main/java/io/marelso/shineyard/ui/list/ListViewModel.kt
+++ app/src/main/java/io/marelso/shineyard/ui/list/ListViewModel.kt
@@ -2,7 +2,39 @@ package io.marelso.shineyard.ui.list
 
 import androidx.lifecycle.ViewModel
 import io.marelso.shineyard.data.Account
+import io.marelso.shineyard.data.Device
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class ListViewModel(private val currentAccount: Account): ViewModel() {
-    val user = currentAccount.email
+class ListViewModel(
+    private val currentAccount: Account,
+    private val repository: DeviceListRepository
+): ViewModel() {
+    private val _devices = MutableStateFlow<List<Device>>(mutableListOf())
+    val devices: StateFlow<List<Device>> = _devices
+
+    init {
+        currentAccount.accountDevices.forEach {
+            it.id?.let {
+                repository.getDevice(it) {
+                    it?.let { device ->
+                        putDeviceIntoList(device)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun putDeviceIntoList(device: Device) {
+        _devices.tryEmit(
+            _devices.value.toMutableList().apply {
+                val existingDeviceIndex = indexOfFirst { it.id == device.id } // Assuming each Device has a unique id
+                if (existingDeviceIndex != -1) {
+                    this[existingDeviceIndex] = device
+                } else {
+                    add(device)
+                }
+            }
+        )
+    }
 }
--- app/src/main/java/io/marelso/shineyard/ui/list/di/ListModule.kt
+++ app/src/main/java/io/marelso/shineyard/ui/list/di/ListModule.kt
@@ -1,14 +1,24 @@
 package io.marelso.shineyard.ui.list.di
 
+import com.google.firebase.database.FirebaseDatabase
 import io.marelso.shineyard.data.Account
 import io.marelso.shineyard.data.Constants
+import io.marelso.shineyard.data.network.FirebaseRepository
+import io.marelso.shineyard.ui.list.DeviceListRepository
 import io.marelso.shineyard.ui.list.ListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val listModule = module {
+    factory {
+        DeviceListRepository(database = FirebaseDatabase.getInstance().getReference("/ESP32_Devices"))
+    }
     viewModel {
-        ListViewModel(get<Account>(named(Constants.KEY_ACCOUNT)))
+        ListViewModel(
+            currentAccount = get<Account>(named(Constants.KEY_ACCOUNT)),
+            repository = get()
+        )
     }
 }
